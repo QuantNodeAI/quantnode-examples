60b606128c21ce3dc63e2ef5493281ca87367d60
Rene Kesler <keksa905@gmail.com>
2022-07-21 18:09:34 +0200

    transactions example

---
+++ table_data/tx_by_address.sql
@@ -0,0 +1,16 @@
+-- Get transactions for given address
+-- CAUTION: DO NOT use 'order by' yet (it is extremely slowing query down)
+
+SELECT confirmed_at,
+       block,
+       tx_hash,
+       (select address from chain_bsc.addresses where id = transactions.from_id) as from_address,
+       (select address from chain_bsc.addresses where id = transactions.to_id)   as to_address,
+       value,
+       tx_fee
+from chain_bsc.transactions
+where from_id = ANY
+      (ARRAY(SELECT id FROM chain_bsc.addresses where address = '0xbaC14A55Da4327f79D69AF657AD6837aA49c9aC6'))
+   or to_id = ANY
+      (ARRAY(SELECT id FROM chain_bsc.addresses where address = '0xbaC14A55Da4327f79D69AF657AD6837aA49c9aC6'))
+limit 10;
