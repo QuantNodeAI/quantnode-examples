c38f776836068cd073b56cf1b40ec3296aaa84dc
Rene Kesler <keksa905@gmail.com>
2022-07-22 13:01:32 +0200

    fix active tokens

--- pie_chart/most_active_tokens.sql
+++ pie_chart/most_active_tokens.sql
@@ -2,7 +2,7 @@
 
 SELECT token_id,
        (select t.symbol from chain_bsc.tokens t where t.id = token_id),
-       count(wallet_id) as unique_wallets
+       count(distinct wallet_id) as unique_wallets
 FROM chain_bsc.balance_move_ticks_hourly
 WHERE bucket > now() - interval '24 hours'
 GROUP BY token_id
