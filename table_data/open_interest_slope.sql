49b1d6a937b23de7b944a2389fb075c1c8ac72b5
jancimertel <10438157+jancimertel@users.noreply.github.com>
2022-12-02 10:45:21 +0100

    fix open_interest_slope.sql for trino

--- table_data/open_interest_slope.sql
+++ table_data/open_interest_slope.sql
@@ -1,9 +1,9 @@
 -- Sort binance Pairs by its slope of Open Interest data through given time interval
 
-SELECT (select symbol from binance.pairs where id = interest.pair_id)            as pair,
-       regr_slope(CAST(open_interest as numeric), extract(epoch from timestamp)) AS slope
+SELECT (select symbol from binance.pairs where id = interest.pair_id) as pair,
+       regr_slope(CAST(open_interest as decimal), to_unixtime(timestamp)) AS slope
 FROM binance.open_interest_15_minutes interest
-where timestamp > '2022-09-30'
-  and timestamp < '2022-10-01'
-GROUP BY pair
+where timestamp > timestamp '2022-09-30'
+  and timestamp < timestamp '2022-10-01'
+GROUP BY pair_id
 ORDER BY slope desc;
