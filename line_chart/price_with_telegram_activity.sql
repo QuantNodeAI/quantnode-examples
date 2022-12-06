3d78e165c993c5cb31613e0b282c9aa9308b339f
jancimertel <10438157+jancimertel@users.noreply.github.com>
2022-12-06 17:27:40 +0100

    update price_with_telegram_activity.sql for trino

--- line_chart/price_with_telegram_activity.sql
+++ line_chart/price_with_telegram_activity.sql
@@ -4,24 +4,17 @@
 WITH token AS (SELECT id,
                       symbol,
                       'eth' as tag
-               FROM chain_eth.tokens
-               WHERE contract = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2')
+               FROM binance.pairs
+               WHERE symbol = 'ETHUSDT')
 SELECT a.bucket, a.price, b.count
-FROM (SELECT p.bucket, last(p.close_stable, p.bucket) as price
-      FROM chain_eth.price_ticks_hourly p,
+FROM (SELECT p.bucket, last_value(p.close_stable) OVER (PARTITION BY bucket) as price
+      FROM binance.price_ticks_hourly p,
            token
-      where p.bucket > '2022-10-05'
-        and p.bucket < '2022-10-06'
+      where p.bucket > timestamp '2022-10-05'
+        and p.bucket < timestamp '2022-10-06'
         and p.token_id = token.id
-      GROUP BY 1
       ORDER BY 1) a
          LEFT JOIN
-     (SELECT time_bucket('1 hour', created_at) as bucket, count(d.id)
-      FROM publications.telegram_data d,
-           token
-      where created_at >= '2022-10-05'
-        and created_at <= '2022-10-06'
-        and d.tag in (token.tag)
-      group by 1
-      order by 1) b
+     (select bucket, count from publications.public.telegram_data_1_hour_count cnt, token 
+      where cnt.tag in (token.tag) and bucket >= timestamp '2022-10-05' and bucket < timestamp '2022-10-06') b
      on a.bucket = b.bucket;
