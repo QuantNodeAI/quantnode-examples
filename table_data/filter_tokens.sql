b665724914400b4426bf5740a7c40582ed4b903b
Rene Kesler <keksa905@gmail.com>
2023-01-06 17:09:39 +0100

    filter_tokens updated

--- table_data/filter_tokens.sql
+++ table_data/filter_tokens.sql
@@ -1,11 +1,15 @@
 -- Get filtered token by some technical indicators with its indicators and patterns as well
-SELECT (select t.symbol from chain_bsc.tokens t where t.id = a.token_id), a.*, tfa.*
-FROM screener.analyses a
-         join screener.time_frame_analyses tfa on a.token_id = tfa.token_id
-WHERE timeframe = 'H1'
-  and market_cap > 20000000
-  and sma20 > sma100
-  and rsi < 50
-  and a.chain = 56
-ORDER BY market_cap DESC
-LIMIT 10;
+WITH analysis AS (SELECT cast(a.market_cap as decimal(38, 10)) as market_cap, tfa.*
+                  FROM agg.screener.analyses a
+                           join agg.screener.time_frame_analyses tfa on a.token_id = tfa.token_id
+                  WHERE timeframe = 'H1'
+                    and cast(market_cap as decimal(38, 10)) > 20000000
+                    and sma20 > sma100
+                    and cast(rsi as decimal(38, 10)) < 50
+                    and a.chain = 1
+                  ORDER BY market_cap DESC
+    LIMIT 10)
+SELECT symbol, analysis.*
+FROM eth.public.tokens,
+     analysis
+WHERE tokens.id = analysis.token_id;
