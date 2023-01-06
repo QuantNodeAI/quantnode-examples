a4b46cd77930214ca9d244b5cc56981eced0ede7
Rene Kesler <keksa905@gmail.com>
2023-01-06 17:11:52 +0100

    tag frequencies removed for now

--- bar_chart/discord_tag_frequency.sql
+++
@@ -1,8 +0,0 @@
--- Get tag frequency in the discord data for given time interval
-
-SELECT tag, count(id) as count
-FROM publications.discord_data
-where created_at >= '2022-07-22'
-  and created_at <= '2022-07-23'
-group by tag
-order by count desc;
--- pie_chart/telegram_tag_frequency.sql
+++
@@ -1,8 +0,0 @@
--- Get tag frequency in all telegram messages for given time interval
-
-SELECT tag, count(id) as count
-FROM publications.telegram_data
-where created_at >= '2022-07-22'
-  and created_at <= '2022-07-23'
-group by tag
-order by count desc;
