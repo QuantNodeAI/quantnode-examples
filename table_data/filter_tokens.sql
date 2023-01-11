92d27cf3c6e19552d94ccb6e07704bd4e968a96e
Rene Kesler <keksa905@gmail.com>
2023-01-11 15:26:54 +0100

    optimized screener queries

--- table_data/filter_tokens.sql
+++ table_data/filter_tokens.sql
@@ -1,15 +1,11 @@
 -- Get filtered token by some technical indicators with its indicators and patterns as well
-WITH analysis AS (SELECT cast(a.market_cap as decimal(38, 10)) as market_cap, tfa.*
-                  FROM screener.screener.analyses a
-                           join screener.screener.time_frame_analyses tfa on a.token_id = tfa.token_id
-                  WHERE timeframe = 'H1'
-                    and cast(market_cap as decimal(38, 10)) > 20000000
-                    and sma20 > sma100
-                    and cast(rsi as decimal(38, 10)) < 50
-                    and a.chain = 1
-                  ORDER BY market_cap DESC
-    LIMIT 10)
-SELECT symbol, analysis.*
-FROM eth.public.tokens,
-     analysis
-WHERE tokens.id = analysis.token_id;
+SELECT *,
+       *symbol('token_id', 'eth', 'tokens')
+FROM screener.screener.indicators
+WHERE timeframe = 'H1'
+  and market_cap > '20000000'
+  and sma20 > sma100
+  and rsi < '50'
+  and chain = 1
+ORDER BY market_cap DESC
+LIMIT 10;
